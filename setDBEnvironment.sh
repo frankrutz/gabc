433c5a691117fce387884f2e4e4276314a92a680
Frank Potthast Rutz <frank.potthast@gmail.com>
2017-04-11 18:26:16 +0200

    Update setDBEnvironment.sh

--- setDBEnvironment.sh
+++ setDBEnvironment.sh
@@ -1,4 +1,4 @@
-export MY_DB_SERVER=my_db
+export MY_DB_SERVER=mydb
 export MY_DB_PASSWD=AM89jkdip98LKMJdkp
 export MY_USER_PASSWD=HodiPok8492mjkop
 export MY_DB_USER=potf0815
@@ -7,3 +7,6 @@ export MY_TEST_TARGET=my_test_goes_here
 
 
 docker run -it --rm -e MY_DB_SERVER=${MY_DB_SERVER} -e MY_DB_PASSWD=${MY_DB_PASSWD} -e MY_USER_PASSWD=${MY_USER_PASSWD} -e MY_DB_USER=${MY_DB_USER} -e MY_TEST_TARGET=${MY_TEST_TARGET} appserver
+
+
+sqlcmd -S mydb -U SA -P AM89jkdip98LKMJdkp -Q "select getDate()";
