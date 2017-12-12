3cfc773582a6903a8f7d6450039c07550e6ff072
Frank Potthast Rutz <frank.potthast@gmail.com>
2017-12-12 04:51:01 +0100

    Update setDBEnvironment.sh

--- setDBEnvironment.sh
+++ setDBEnvironment.sh
@@ -1,12 +1,13 @@
-export MY_DB_SERVER=mydb
+export MY_DB_SERVER=localhost
 export MY_DB_PASSWD=AM89jkdip98LKMJdkp
 export MY_USER_PASSWD=HodiPok8492mjkop
-export MY_DB_USER=potf0815
+export MY_DB_USER=tmpMyDbUser
+export MY_DB=tmpMyDb
 export MY_TEST_TARGET=my_test_goes_here
 
 
 
-docker run -it --rm -e MY_DB_SERVER=${MY_DB_SERVER} -e MY_DB_PASSWD=${MY_DB_PASSWD} -e MY_USER_PASSWD=${MY_USER_PASSWD} -e MY_DB_USER=${MY_DB_USER} -e MY_TEST_TARGET=${MY_TEST_TARGET} appserver
+docker run -it --rm  -e MY_DB=${MY_DB}-e MY_DB_SERVER=${MY_DB_SERVER} -e MY_DB_PASSWD=${MY_DB_PASSWD} -e MY_USER_PASSWD=${MY_USER_PASSWD} -e MY_DB_USER=${MY_DB_USER} -e MY_TEST_TARGET=${MY_TEST_TARGET} appserver
 
 
 sqlcmd -S mydb -U SA -P AM89jkdip98LKMJdkp -Q "select getDate()";
